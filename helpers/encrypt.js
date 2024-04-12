80fbd72f38e26e4245835437356affb041ebf120
krstf <kristof.krasznai@trendency.hu>
2024-04-12 09:54:41 +0200

    initial

--- helpers/encrypt.js
+++ helpers/encrypt.js
@@ -5,18 +5,13 @@ async function hashPassword (password) {
 
     const saltRounds = 10;
 
-    const hashedPassword = await new Promise((resolve, reject) => {
+    return await new Promise((resolve, reject) => {
         bcrypt.hash(password, saltRounds, function(err, hash) {
             if (err) reject(err)
             resolve(hash)
         });
     })
 
-    return hashedPassword
-}
-
-function compareHashes(){
-    const isSame = bcrypt.compare()
 }
 
 module.exports = {
